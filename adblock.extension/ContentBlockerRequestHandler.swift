e1dc0633f27f0053df2c2848e3561827cbe93dd5
DEVBUFF <aliengr0001@gmail.com>
2020-10-07 17:56:25 +1100

    Initial Commit

---
+++ adblock.extension/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  adblock.extension
+//
+//  Created by Igor Ryazancev on 6/26/20.
+//  Copyright © 2020 DEVBUFF. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
